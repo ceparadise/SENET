6646c47f635de7c9d4705b42de091792867634a6
yalin liu <yalin@brandeis.edu>
2017-10-22 16:49:32 -0400

    print keys not in the model

--- src/scripts/count_keyword.py
+++ src/scripts/count_keyword.py
@@ -7,15 +7,22 @@ with open("../../data/vocabulary.txt", encoding="utf8") as fin:
 count = 0
 model = Word2Vec.load("../../data/w2v.model")
 in_vocab = set()
+un_vocab = set()
 vocab = model.wv.vocab.keys()
 for key in keywords:
-    key = key.strip(' \n\t').replace(' ','_')
+    key = key.strip(' \n\t').replace(' ', '_')
     if key in vocab or key + "\n" in vocab:
         in_vocab.add(key)
         count += 1
+    else:
+        un_vocab.add(key)
 
 with open("./appearedWords.txt", 'w') as out:
     for word in in_vocab:
+        out.write(word + "\n")
+
+with open("./notAppearedWords.txt", "w") as out:
+    for word in un_vocab:
         out.write(word + "\n")
 
 print("vocab size {} , and {} keywords are in the vocab".format(len(vocab), len(in_vocab), count))
