e5c4fbdb2a37a8896eeb5d5a3ee21f2d364527e2
yalin liu <yalin@brandeis.edu>
2017-10-22 20:14:01 -0400

    find words with no definition

---
+++ src/scripts/find_word_with_no_def.py
@@ -0,0 +1,8 @@
+from .common import *
+import os
+
+with open(DATA_DIR + os.sep + "vocabulary_wiki.txt") as fin:
+    for line in fin:
+        parts = line.split("\t")
+        if len(parts == 1):
+            print(parts[0])
