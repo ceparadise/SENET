7fb08885fcf07f19177d5c651a1418dfbd548cb9
yalin liu <yalin@brandeis.edu>
2018-02-14 00:53:39 -0500

    do more cleaning in vocab

--- src/scripts/clean_vocab.py
+++ src/scripts/clean_vocab.py
@@ -4,13 +4,15 @@ Clean the vocabulary file
 from common import *
 import re
 
-vocab_file = VOCAB_DIR + os.sep + "vocabulary.txt"
-vocab_clean = VOCAB_DIR + os.sep + "vocabulary_clean.txt"
+vocab_file = VOCAB_DIR + os.sep + "vocabulary_origin.txt"
+vocab_clean = VOCAB_DIR + os.sep + "vocabulary.txt"
 
 with open(vocab_file, 'r', encoding='utf8') as fin, open(vocab_clean, 'w', encoding='utf8') as fout:
     for line in fin:
         line = line.strip("\n\t\r ")
         line = re.sub('\(.*?\)', '', line)
+        line = re.sub("\s+"," ", line)
+        line = re.sub("\s*-\s*","-", line)
         tokens = line.split(" ")
         tokens = [x for x in tokens if len(x) > 0]
         word = " ".join(tokens)
